ba572ce8afcdb35b1d0cfb00000a7ba6d71570ff
Revanth <revanthanchoori8@gmail.com>
2021-03-04 14:33:19 +0530

    Ranks Added

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -31,5 +32,16 @@ class Player {
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
     })
+  }
+  getCarsAtEnd(){
+    var carsAtEndRef = database.ref('carsAtEnd');
+    carsAtEndRef.on("value", (data)=>{
+      this.rank = data.val();
+    })
+  }
+  updateCarsAtEnd(rank){
+    database.ref('/').update({
+      carsAtEnd: rank
+    });
   }
 }
